55445559515be26459e920d23edbaa496b8169d6
Vikktor93 <Vikktor93@users.noreply.github.com>
2023-04-18 12:48:27 -0400

    Se actualiza las funciones de suma y resta

--- Labs/laboratorio1-random.py
+++ Labs/laboratorio1-random.py
@@ -1,28 +1,17 @@
 import random
 
-# Función para crear una matriz aleatoria
+#Función para crear una matriz aleatoria
 def matriz_random(filas, columnas):
-    matriz = []
+    m = []
     for i in range(filas):
         fila = []
         for j in range(columnas):
-            elemento = random.randint(1, 5) # generamos un número aleatorio entre 1 y 10
-            fila.append(elemento)
-        matriz.append(fila)
-    return matriz
+            elem = random.randint(1, 5) #número aleatorio entre 1 y 5
+            fila.append(elem)
+        m.append(fila)
+    return m
 
-# Función para sumar dos matrices
-def suma_matrices(m1, m2):
-    matriz_final = []
-    for i in range(len(m1)):
-        fila = []
-        for j in range(len(m1[0])):
-            suma = m1[i][j] + m2[i][j]
-            fila.append(suma)
-        matriz_final.append(fila)
-    return matriz_final
-
-# Función para restar dos matrices
+#Resta de 2 matrices
 def resta_matrices(m1, m2):
     matriz_final = []
     for i in range(len(m1)):
@@ -33,9 +22,20 @@ def resta_matrices(m1, m2):
         matriz_final.append(fila)
     return matriz_final
 
-# Creamos dos matrices aleatorias con el mismo tamaño
-filas = int(input("Ingrese la cantidad de filas: "))
-columnas = int(input("Ingrese la cantidad de columnas: "))
+#Suma de 2 matrices
+def suma_matrices(m1, m2):
+    matriz_final = []
+    for i in range(len(m1)):
+        fila = []
+        for j in range(len(m1[0])):
+            suma = m1[i][j] + m2[i][j]
+            fila.append(suma)
+        matriz_final.append(fila)
+    return matriz_final
+
+#Se crean 2 matrices random de un mismo tamaño
+filas = int(input("Ingrese la cantidad de filas de la matriz: "))
+columnas = int(input("Ingrese la cantidad de columnas de la matriz: "))
 print("Creando la primera matriz aleatoria:")
 m1 = matriz_random(filas, columnas)
 print(m1,"\n")
@@ -43,14 +43,15 @@ print("Creando la segunda matriz aleatoria:")
 m2 = matriz_random(filas, columnas)
 print(m2,"\n")
 
-# Sumamos las dos matrices y mostramos el resultado
-matriz_final = suma_matrices(m1, m2)
-print("La suma de las matrices es:")
+#Resta de las 2 matrices y se imprime la matriz resultante
+matriz_final = resta_matrices(m1, m2)
+print("La resta de las matrices da como resultado la siguiente matriz:")
 for fila in matriz_final:
     print(fila,"\n")
 
-# Restamos las dos matrices y mostramos el resultado
-matriz_final = resta_matrices(m1, m2)
-print("La resta de las matrices es:")
+#Suma de las 2 matrices y se imprime la matriz resultante
+matriz_final = suma_matrices(m1, m2)
+print("La suma de las matrices da como resultado la siguiente matriz:")
 for fila in matriz_final:
     print(fila)
+
