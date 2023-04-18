931c0c0b579340815ded0dd7c248dba4bfe2f73d
Vikktor93 <Vikktor93@users.noreply.github.com>
2023-04-18 12:42:19 -0400

    Actualización funciones Lab 1

--- Labs/laboratorio1-numpy.py
+++ Labs/laboratorio1-numpy.py
@@ -1,39 +1,41 @@
 import numpy as np
 
-#Función para pedir los elementos de una matriz por teclado
-def pedir_matriz(filas, columnas):
-    matriz = np.zeros((filas, columnas))
+#Función que solicita los elementos de la matriz por consola
+def elementos_matriz(filas, columnas):
+    m = np.zeros((filas, columnas))
     for i in range(filas):
         for j in range(columnas):
-            matriz[i][j] = float(input(f"Elemento ({i+1}, {j+1}): "))
-    return matriz
+            m[i][j] = int(input(f"Elemento ({i+1}, {j+1}): "))
+    return m
 
-# Función para sumar dos matrices
-def sumar_matrices(matriz1, matriz2):
-    return matriz1 + matriz2
+#Resta de las 2 matrices
+def resta_matrices(m1, m2):
+    return m1 - m2
 
-# Función para restar dos matrices
-def restar_matrices(matriz1, matriz2):
-    return matriz1 - matriz2
+#Suma de las 2 matrices
+def suma_matrices(m1, m2):
+    return m1 + m2
 
-# Pedimos la cantidad de filas y columnas para ambas matrices
-filas = int(input("Ingrese la cantidad de filas: "))
-columnas = int(input("Ingrese la cantidad de columnas: "))
+#Se solicita la cantidad de filas y columnas por consola
+filas = int(input("Ingrese la cantidad de filas de la matriz: "))
+columnas = int(input("Ingrese la cantidad de columnas de la matriz: "))
 
-# Creamos la primera matriz ingresando sus valores por teclado
+#Se crea la primera matriz
 print("Ingrese los elementos de la primera matriz:")
-matriz1 = pedir_matriz(filas, columnas)
+m1 = elementos_matriz(filas, columnas)
 
-# Creamos la segunda matriz ingresando sus valores por teclado
+#Se crea la segunda matriz
 print("Ingrese los elementos de la segunda matriz:")
-matriz2 = pedir_matriz(filas, columnas)
+m2 = elementos_matriz(filas, columnas)
 
-# Sumamos las dos matrices y mostramos el resultado
-matriz_resultante = sumar_matrices(matriz1, matriz2)
-print("La suma de las matrices es:")
-print(matriz_resultante)
 
-# Restamos las dos matrices y mostramos el resultado
-matriz_resultante = restar_matrices(matriz1, matriz2)
+#Se resta las 2 matrices y se imprime
+matriz_final = resta_matrices(m1, m2)
 print("La resta de las matrices es:")
-print(matriz_resultante)
+print(matriz_final)
+
+#Se suma las 2 matrices y se imprime
+matriz_final = suma_matrices(m1, m2)
+print("La suma de las matrices es:")
+print(matriz_final)
+
